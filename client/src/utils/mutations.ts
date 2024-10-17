26ac7503c8103bbfda4cbfca9648ee46f84fdc2c
[mollyhoward] <mollyhoward319@gmail.com>
2024-10-17 13:18:30 -0500

    edit mutations from example

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,45 +1,58 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($input: ProfileInput!) {
-    addProfile(input: $input) {
+export const ADD_USER = gql`
+  mutation addUser($input: UserInput!) {
+    addUser(input: $input) {
       token
-      profile {
+      user {
         _id
-        name
+        username
       }
     }
   }
 `;
 
-export const ADD_SKILL = gql`
-  mutation addSkill($profileId: ID!, $skill: String!) {
-    addSkill(profileId: $profileId, skill: $skill) {
-      _id
-      name
-      skills
-    }
-  }
-`;
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      profile {
+      user {
         _id
-        name
+        username
+        email
       }
     }
   }
 `;
 
-export const REMOVE_SKILL = gql`
-  mutation removeSkill($skill: String!) {
-    removeSkill(skill: $skill) {
+export const ADD_BOOK = gql`
+  mutation addBook($input: BookInput!) {
+    addBook(input: $input) {
       _id
-      name
-      skills
+      username
+      savedBooks {
+        bookID
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookID: String!) {
+    removeBook(bookID: $bookID) {
+      _id
+    bookID
+    title
+    authors
+    description
+    image
+    link
     }
   }
 `;
